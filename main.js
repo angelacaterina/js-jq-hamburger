d4ebf460887a70509d2a6c1385ccb7e04b7d46c6
angelacaterina <angela.caterina@icloud.com>
2020-11-26 15:38:36 +0100

    revisione
    ho aggiunto delle variabili per capire miglio il codice in jQuery, ho lasciato entrambi i metodi, il primo senza le variabili mentre il secondo con le variabili

--- main.js
+++ main.js
@@ -3,10 +3,25 @@
 // (ricordate che per vedere l’hamburger in alto a destra dovete scendere a risoluzioni tipo tablet o mobile).
 
 // $('selector').click(); // associa ad un click una funzione
-$('div.header-right > a').click(function () {
-  $('.hamburger-menu').show();
+
+// METODO 1: senza dichiarare variabili
+// ('div.header-right > a').click(function () {
+//   $('.hamburger-menu').show();
+// });
+//
+// $('.hamburger-menu >a.close').click(function () {
+//   $('.hamburger-menu').hide();
+// });
+
+// METODO 2: dichiarando le variabili, per renderlo più comprensibile
+var headerRightIcon = $("div.header-right > a");
+var hamburgerMenu = $(".hamburger-menu");
+var hamburgerMenuIconClose = $(".hamburger-menu >a.close");
+
+headerRightIcon.click(function () {
+  hamburgerMenu.show();
 });
 
-$('.hamburger-menu >a.close').click(function () {
-  $('.hamburger-menu').hide();
+hamburgerMenuIconClose.click(function () {
+  hamburgerMenu.hide();
 });
